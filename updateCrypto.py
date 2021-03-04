0c0d990230c54722672f0854e113f8ee19e8ed32
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2021-03-04 13:45:17 -0600

    started on daily summary script

--- updateCrypto.py
+++ updateCrypto.py
@@ -4,85 +4,86 @@ import json
 import pandas
 from datetime import date, datetime
 
+def updateCryptoData():
 
-today = datetime.now()
-date = today.strftime("%d/%m/%Y")
-time = today.strftime("%H:%M:%S")
-print(date, time)
+    today = datetime.now()
+    date = today.strftime("%d/%m/%Y")
+    time = today.strftime("%H:%M:%S")
+    print(date, time)
 
-df = pandas.read_csv('crypto-log.csv')
-df = df.dropna(how='all')
+    df = pandas.read_csv('crypto-log.csv')
+    df = df.dropna(how='all')
 
-bp = df.groupby(['Coin'])['buy price'].sum()
+    bp = df.groupby(['Coin'])['buy price'].sum()
 
-ppc = {}
-for key in df.Coin.unique():
-    if key == key:
-        pdf = df.loc[df['Coin'] == key]
-        ppc[key] = (pdf['buy price'] * pdf['price per coin']).sum()/pdf['buy price'].sum()
+    ppc = {}
+    for key in df.Coin.unique():
+        if key == key:
+            pdf = df.loc[df['Coin'] == key]
+            ppc[key] = (pdf['buy price'] * pdf['price per coin']).sum()/pdf['buy price'].sum()
 
-cc = df.groupby(['Coin'])['How much'].sum()
+    cc = df.groupby(['Coin'])['How much'].sum()
 
 
-oldCoinValue = {}
-
-for coin in bp.keys():
-    # filter out nan
-    if coin == coin:
-        oldCoinValue[coin] = abs(bp[coin]) 
-        
-allCoins = ''.join([k +',' for k in bp.keys()])
-allCoins = allCoins[:-1:]
-
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-parameters = {
-    'symbol':allCoins,
-    'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': open('cmc_key.txt').read(),
-}
-
-session = Session()
-session.headers.update(headers)
-
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    newCoinValues = {}
-        
-    coinChange = {}
+    oldCoinValue = {}
 
     for coin in bp.keys():
-        newCoinValues[coin] = data['data'][coin]['quote']['USD']['price'] * cc[coin]
-        coinChange[coin] = ((newCoinValues[coin] - oldCoinValue[coin]) / oldCoinValue[coin]) * 100
-    
-    ndf = pandas.DataFrame(columns=['Date (d/m/y)', 'Time', 'Coin', 'Quantity', 'Old value', 'New value', 'Avg ppc', 'Curr ppc', 'Value change ($)', 'Value change (%)'])
-    
-    for i, coin in enumerate(coinChange.keys()):
-        ndf.loc[i] = [date, time, coin, cc[coin], oldCoinValue[coin], newCoinValues[coin], ppc[coin],  data['data'][coin]['quote']['USD']['price'], newCoinValues[coin] - oldCoinValue[coin], coinChange[coin]]
-    print(ndf)
-    
-    toPrint = ''
-    try:
-        odf = pandas.read_csv('allData.csv')
-        toPrint = pandas.concat([odf, ndf])
-    except:
-        toPrint = ndf
-    toPrint.to_csv('allData.csv', columns=['Date (d/m/y)', 'Time', 'Coin', 'Quantity', 'Old value', 'New value', 'Avg ppc', 'Curr ppc'], index=False)
-    
-    totalChange = (sum(newCoinValues.values()) - sum(oldCoinValue.values()))/sum(oldCoinValue.values()) * 100
-    if totalChange >=0:
-        print('\nTotal:\t$' + str(sum(newCoinValues.values()) - sum(oldCoinValue.values())) + '\t+' + str(totalChange) + ' %')
-    else:
-        print('\nTotal:\t$' + str(sum(newCoinValues.values()) - sum(oldCoinValue.values())) + '\t' + str(totalChange) + ' %')
-    
-    print('Actual change: ' + str(sum(oldCoinValue.values())) + ' -> ' + str(sum(newCoinValues.values())))
-    
+        # filter out nan
+        if coin == coin:
+            oldCoinValue[coin] = abs(bp[coin]) 
+            
+    allCoins = ''.join([k +',' for k in bp.keys()])
+    allCoins = allCoins[:-1:]
 
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'symbol':allCoins,
+        'convert':'USD'
+    }
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': open('cmc_key.txt').read(),
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        newCoinValues = {}
+            
+        coinChange = {}
+
+        for coin in bp.keys():
+            newCoinValues[coin] = data['data'][coin]['quote']['USD']['price'] * cc[coin]
+            coinChange[coin] = ((newCoinValues[coin] - oldCoinValue[coin]) / oldCoinValue[coin]) * 100
+        
+        ndf = pandas.DataFrame(columns=['Date (d/m/y)', 'Time', 'Coin', 'Quantity', 'Old value', 'New value', 'Avg ppc', 'Curr ppc', 'Value change ($)', 'Value change (%)'])
+        
+        for i, coin in enumerate(coinChange.keys()):
+            ndf.loc[i] = [date, time, coin, cc[coin], oldCoinValue[coin], newCoinValues[coin], ppc[coin],  data['data'][coin]['quote']['USD']['price'], newCoinValues[coin] - oldCoinValue[coin], coinChange[coin]]
+        print(ndf)
+        
+        toPrint = ''
+        try:
+            odf = pandas.read_csv('allData.csv')
+            toPrint = pandas.concat([odf, ndf])
+        except:
+            toPrint = ndf
+        toPrint.to_csv('allData.csv', columns=['Date (d/m/y)', 'Time', 'Coin', 'Quantity', 'Old value', 'New value', 'Avg ppc', 'Curr ppc'], index=False)
+        
+        totalChange = (sum(newCoinValues.values()) - sum(oldCoinValue.values()))/sum(oldCoinValue.values()) * 100
+        if totalChange >=0:
+            print('\nTotal:\t$' + str(sum(newCoinValues.values()) - sum(oldCoinValue.values())) + '\t+' + str(totalChange) + ' %')
+        else:
+            print('\nTotal:\t$' + str(sum(newCoinValues.values()) - sum(oldCoinValue.values())) + '\t' + str(totalChange) + ' %')
+        
+        print('Actual change: ' + str(sum(oldCoinValue.values())) + ' -> ' + str(sum(newCoinValues.values())))
+        
+
+        
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
     
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
-  
 
--- viewDaily.py
+++ viewDaily.py
@@ -1,2 +1,47 @@
 import pandas
+from datetime import datetime, date, timedelta
+from updateCrypto import updateCryptoData
+import regex as re
+
+# updateCryptoData()
+print('-------------- end update -----------------')
+
+data = pandas.read_csv('allData.csv')
+
+today = datetime.now()
+date = today.strftime("%d/%m/%Y")
+time = today.strftime("%H:%M:%S")
+
+yesterday = today - timedelta(days=1)
+yesterDate = yesterday.strftime("%d/%m/%Y")
+
+tdf = data.loc[data['Date (d/m/y)'] == date]
+ydf = data.loc[data['Date (d/m/y)'] == yesterDate]
+
+# print(tdf)
+# print(ydf)
+
+# select where times are the same to whatever accuracy is possible
+sameTimes = tdf.loc[tdf['Time'].astype(str) == time]
+numsIncluded = 8
+while sameTimes.empty and numsIncluded > 1:
+    sameTimes = tdf.loc[tdf['Time'].astype(str).str[:numsIncluded:] == time[:numsIncluded:]]
+    numsIncluded -=1
+    
+ysameTimes = ydf.loc[ydf['Time'].astype(str) == time]
+numsIncluded = 8
+while ysameTimes.empty and numsIncluded > 1:
+    ysameTimes = ydf.loc[ydf['Time'].astype(str).str[:numsIncluded:] == time[:numsIncluded:]]
+    numsIncluded -=1
+    
+print(sameTimes)
+print(ysameTimes)
+
+
+
+
+
+
+
+
 
